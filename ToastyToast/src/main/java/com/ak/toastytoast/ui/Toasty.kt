f0f6069587c53052eb1c699d4a84680c892b1977
VisakhC <visakh@meridian.net.in>
2022-04-24 15:47:57 +0530

    working on toast

--- ToastyToast/src/main/java/com/ak/toastytoast/ui/Toasty.kt
+++ ToastyToast/src/main/java/com/ak/toastytoast/ui/Toasty.kt
@@ -1,34 +1,124 @@
 package com.ak.toastytoast.ui
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import coil.loadAny
 import com.ak.toastytoast.R
+import com.ak.toastytoast.utils.show
 
 object Toasty {
-    val LENGTH_SHORT = Toast.LENGTH_SHORT
-    val LENGTH_LONG = Toast.LENGTH_LONG
+    const val SHORT = Toast.LENGTH_SHORT
+    const val LONG = Toast.LENGTH_LONG
+
+
+    var duration: Int = SHORT
+    var layout: Int = R.layout.layout_toasty
+    var tint: Int = R.color.white
+    var drawable: Any? = null
+    var textColor = Color.BLACK
+    var text: String? = null
+    lateinit var toast: Toast
+    var gravity: Int? = null /*toast?.gravity*/
+    var xOffset: Int? = null /*toast?.xOffset*/
+    var yOffset: Int? = null /*toast?.yOffset*/
+
+
     private var lastToast: Toast? = null
     private const val allowQueue = true
 
-    fun success(context: Context, message: String, duration: Int):Toast {
+
+    @SuppressLint("InflateParams")
+    fun success(
+        context: Context
+    ): Toast {
+        val baseToast = Toast.makeText(context, "", duration)
+
+        val toastLayout: View =
+            (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+                .inflate(layout, null)
+        toastLayout.background.setTint(ContextCompat.getColor(context, tint))
+
+        val iconView = toastLayout.findViewById<ImageView>(R.id.iv_icon)
+        if (drawable != null) {
+            iconView.loadAny(drawable)
+            iconView.show()
+        }
+
+        val messageTextView = toastLayout.findViewById<TextView>(R.id.tv_message)
+        messageTextView.setTextColor(textColor)
+        messageTextView.text = text
+
+        baseToast.view = toastLayout
+
+
+        toast = Toast(context)
+
+
+
+
+
+        if (gravity == null && xOffset == null && yOffset == null) {
+            baseToast.setGravity(Gravity.CENTER_HORIZONTAL or Gravity.BOTTOM, 0, 0)
+
+        } else if (gravity == null && xOffset == null && yOffset != null) {
+            baseToast.setGravity(Gravity.BOTTOM, toast.xOffset, yOffset!!)
+
+        } else if (gravity == null && xOffset != null && yOffset == null) {
+            baseToast.setGravity(Gravity.BOTTOM, xOffset!!, toast.yOffset)
+
+        } else if (gravity == null && xOffset != null && yOffset != null) {
+            baseToast.setGravity(Gravity.BOTTOM, toast.xOffset, yOffset!!)
+
+        } else if (gravity != null && xOffset == null && yOffset == null) {
+            baseToast.setGravity(gravity!!, toast.xOffset, toast.yOffset)
+
+        } else if (gravity != null && xOffset == null && yOffset != null) {
+            baseToast.setGravity(gravity!!, toast.xOffset, yOffset!!)
+
+        } else if (gravity != null && xOffset != null && yOffset == null) {
+            baseToast.setGravity(gravity!!, xOffset!!, toast.yOffset)
+
+        } else if (gravity != null && xOffset != null && yOffset != null) {
+            baseToast.setGravity(gravity!!, xOffset!!, yOffset!!)
+
+        }
+
+        if (allowQueue) {
+            if (lastToast != null) lastToast?.cancel()
+            lastToast = baseToast
+        }
+        return baseToast
+    }
+
+    /*fun success(
+        context: Context,
+        text: String,
+        duration: Int = SHORT,
+        customLayout: Int = R.layout.layout_toasty,
+        tint: String = "#4CAF50"
+    ): Toast {
         val currentToast = Toast.makeText(context, "", duration)
 
         val toastLayout: View =
             (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
-                .inflate(R.layout.layout_toasty, null)
+                .inflate(customLayout, null)
 
-        toastLayout.background.setTint(ContextCompat.getColor(context, R.color.green))
+        toastLayout.background.setTint(Color.parseColor(tint))
 
         val icon = toastLayout.findViewById<ImageView>(R.id.iv_icon)
         val messageTextView = toastLayout.findViewById<TextView>(R.id.tv_message)
-        messageTextView.text = message
+        messageTextView.text = text
 
         currentToast.view = toastLayout
+
         if (allowQueue) {
             if (lastToast != null) lastToast?.cancel()
             lastToast = currentToast
@@ -36,8 +126,8 @@ object Toasty {
 
         return currentToast
     }
-
-    fun error(context: Context, message: String, duration: Int):Toast {
+*/
+    fun error(context: Context, message: String, duration: Int): Toast {
         val currentToast = Toast.makeText(context, "", duration)
         val toastLayout: View =
             (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
@@ -57,7 +147,7 @@ object Toasty {
         return currentToast
     }
 
-    fun warning(context: Context, message: String, duration: Int):Toast {
+    fun warning(context: Context, message: String, duration: Int): Toast {
         val currentToast = Toast.makeText(context, "", duration)
         val toastLayout: View =
             (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
--- app/src/main/java/com/ak/sampletoast/MainActivity.kt
+++ app/src/main/java/com/ak/sampletoast/MainActivity.kt
@@ -1,25 +1,49 @@
 package com.ak.sampletoast
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
+import android.widget.Toast
 import com.ak.toastytoast.ui.Toasty
+import com.ak.toastytoast.ui.Toasty.LONG
+import com.ak.toastytoast.ui.Toasty.SHORT
+import com.ak.toastytoast.ui.Toasty.drawable
+import com.ak.toastytoast.ui.Toasty.duration
+import com.ak.toastytoast.ui.Toasty.gravity
+import com.ak.toastytoast.ui.Toasty.layout
+import com.ak.toastytoast.ui.Toasty.text
+import com.ak.toastytoast.ui.Toasty.textColor
+import com.ak.toastytoast.ui.Toasty.tint
+import com.ak.toastytoast.ui.Toasty.xOffset
+import com.ak.toastytoast.ui.Toasty.yOffset
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         findViewById<Button>(R.id.btn_success).setOnClickListener {
-            Toasty.success(this@MainActivity, "success_message", Toasty.LENGTH_SHORT).show()
+            Toasty.success(this).also {
+                textColor = Color.BLUE
+                duration = LONG
+                text = "test"
+                duration = SHORT
+                layout = R.layout.test
+                tint = R.color.black
+             //   gravity=Gravity.TOP
+                yOffset=600
+                drawable = R.drawable.ic_android_black_24dp
+            }.show()
+
         }
 
         findViewById<Button>(R.id.btn_error).setOnClickListener {
-            Toasty.error(this@MainActivity, "error", Toasty.LENGTH_SHORT).show()
-         }
+            Toasty.error(this@MainActivity, "error", Toasty.SHORT).show()
+        }
 
         findViewById<Button>(R.id.btn_warning).setOnClickListener {
-            Toasty.warning(this@MainActivity, "warning", Toasty.LENGTH_SHORT).show()
+            Toasty.warning(this@MainActivity, "warning", Toasty.SHORT).show()
         }
     }
 }
